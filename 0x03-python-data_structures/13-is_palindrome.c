98d1547129c0848460da854e9bd70d4ba8a77a79
haddyadnan <haddyadnan@gmail.com>
2022-06-07 06:14:06 +0100

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks palindrome
+ *
+ * @head - list
+ * Return 1
+ */
+
+int is_palindrome(listint_t **head)
+{
+	if (*head == None)
+		return (1);
+}
