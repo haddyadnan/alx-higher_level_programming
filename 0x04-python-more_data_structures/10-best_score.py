5535272ae71c3997bc883c2923c3cd0e843ca697
haddyadnan <haddyadnan@gmail.com>
2022-06-09 05:14:59 +0100

    updt

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 
 def best_score(my_dict):
-    if my_dict is None or best_score == {}:
+    if my_dict is None or my_dict == {}:
         return None
     best_score = max(my_dict.values())
     for key, value in my_dict.items():
