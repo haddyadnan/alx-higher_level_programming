623fde8929fbdff369897bc322efc81068b38f63
haddyadnan <haddyadnan@gmail.com>
2022-06-09 04:41:29 +0100

    inters

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,5 +4,5 @@
 def square_matrix_simple(matrix=[]):
     squares = []
     for i in matrix:
-        squares.append(list(map(lambda i: i**2)))
+        squares.append(list(map(lambda i: i**2, i)))
     return squares
