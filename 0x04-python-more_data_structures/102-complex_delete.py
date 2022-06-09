ff8d122475ae24f6059d78c91619da2d3631849a
haddyadnan <haddyadnan@gmail.com>
2022-06-09 05:32:02 +0100

    del

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def complex_delete(my_dict, value):
+    store = []
+    for key, key_value in my_dict.items():
+        if key_value is value:
+            store.append(key)
+    for x in store:
+        del my_dict[x]
+    return my_dict
