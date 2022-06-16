f0ee34fbf89128ccd7100e89898e74ae2370de5c
haddyadnan <haddyadnan@gmail.com>
2022-06-16 18:51:52 +0100

    code style

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,22 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-
-    """
-    ROUGH TRIAL:
-    -   TO BE OPTIMIZED
-    """
     if type(roman_string) == str:
 
-        roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        roman_list = list(roman_string)
-        n = len(roman_list)
+        r_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500}
+        r_list = list(roman_string)
+        n = len(r_list)
         if n < 1:
-            return roman_list
+            return r_list
         else:
-            total = 0
+            ttl = 0
             for i in range(n + 1):
                 if i == (n - 1):
-                    total += roman_dict[roman_list[i]]
+                    ttl += r_dict[r_list[i]]
                     break
-                elif roman_dict[roman_list[i]] >= roman_dict[roman_list[i + 1]]:
-                    total += roman_dict[roman_list[i]]
+                elif r_dict[r_list[i]] >= r_dict[r_list[i + 1]]:
+                    ttl += r_dict[r_list[i]]
                 else:
-                    total -= roman_dict[roman_list[i]]
-            return total
+                    ttl -= r_dict[r_list[i]]
+            return ttl
     else:
         return 0
