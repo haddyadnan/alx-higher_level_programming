7bf9409100878f0942d6f82101779b9e19099a7e
haddyadnan <haddyadnan@gmail.com>
2022-06-16 18:43:34 +0100

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+
+    """
+    ROUGH TRIAL:
+    -   TO BE OPTIMIZED
+    """
+    if type(roman_string) == str:
+
+        roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        roman_list = list(roman_string)
+        n = len(roman_list)
+        if n < 1:
+            print(roman_list)
+        else:
+            total = 0
+            for i in range(n + 1):
+                if i == (n - 1):
+                    total += roman_dict[roman_list[i]]
+                    break
+                elif roman_dict[roman_list[i]] >= roman_dict[roman_list[i + 1]]:
+                    total += roman_dict[roman_list[i]]
+                else:
+                    total -= roman_dict[roman_list[i]]
+            print(total)
+    else:
+        return 0
