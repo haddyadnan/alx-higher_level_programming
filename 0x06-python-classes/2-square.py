815ad5ee036d63de1a1588a76cfc49a0d9e594d6
haddyadnan <haddyadnan@gmail.com>
2022-06-21 23:24:55 +0100

    square 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Class Square that defines a square """
+
+
+class Square:
+    """
+    Class: Square
+    Attribute: __size (private instance)
+    """
+
+    def __init__(self, size=0):
+
+        """Instance of Class Square"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
