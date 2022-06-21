ba865f30360e8403d8d253a2ad26b2bccca669a7
haddyadnan <haddyadnan@gmail.com>
2022-06-21 23:12:04 +0100

    pycode style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
+
 """ Define a class - Square"""
 
+
 class Square:
-	"""
-	Class: square
-	attribute: __size
-	"""
-	def __init__(self, size):
-		self.__size = size
+    """Class: square attribute: __size"""
+
+    def __init__(self, size):
+        self.__size = size
