668c80fd81238bef02bc16a9ca213983b0d77972
haddyadnan <haddyadnan@gmail.com>
2022-06-21 04:53:30 +0100

    except error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,6 +12,6 @@ def safe_print_list(my_list=[], x=0):
             else:
                 print()
                 return i
-        except:
+        except IndexError:
             print()
             return i
