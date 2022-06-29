fdbd7bccb8bb9ba985e3cee02ffe71d95a45d44e
haddyadnan <haddyadnan@gmail.com>
2022-06-29 12:32:16 +0100

    text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This module prints a text with 2 new lines after each of these characters: .,? and :
+"""
+
+
+def text_indentation(text: str):
+
+    """
+    Print 2 line after occurence of [.,:,?]
+    Return:
+        (string) text
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    hold = ""
+    for txt in text:
+        if txt in [".", "?", ":"]:
+            hold += f"{txt}\n\n"
+        else:
+            hold += hold.join(txt)
+    print(hold.strip("\n"))
