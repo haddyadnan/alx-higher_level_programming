04efacc9af3a6608532283cb6591ca4566b5c3a0
haddyadnan <haddyadnan@gmail.com>
2022-06-29 10:55:48 +0100

    rename folder

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+"""
+This module prints My name is <first_name> <last_name>
+"""
+
+
+def say_my_name(first_name, last_name=""):
+
+    """
+    Prints My name is <first_name> <last_name>
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print(f"My name is {first_name} {last_name}")
