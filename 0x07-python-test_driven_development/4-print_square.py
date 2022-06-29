3509168d9168a6761067cd2b4045751188671d97
haddyadnan <haddyadnan@gmail.com>
2022-06-29 11:24:51 +0100

    print square, doctest

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+"""
+This module contains a function that prints a square with the character #
+"""
+
+
+def print_square(size):
+
+    """
+    Doct
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    char = "#"
+    char *= size
+    for i in range(size):
+        print(char)
