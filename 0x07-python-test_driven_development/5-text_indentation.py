4448f0a12ed6d14381ebd00810314c470130d6a3
haddyadnan <haddyadnan@gmail.com>
2022-06-29 12:33:51 +0100

    code style

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-This module prints a text with 2 new lines after each of these characters: .,? and :
+This module prints a text with 2 new lines \
+after each of these characters: .,? and :
 """
 
 
