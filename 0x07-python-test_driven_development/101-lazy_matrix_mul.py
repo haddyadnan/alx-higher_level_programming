c3ed91f7fbb8a5d50157dcec5372a6e21305f3ae
haddyadnan <haddyadnan@gmail.com>
2022-06-30 02:01:04 +0100

    lazy matrix mul

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+"""
+This module perform dot multiplication of 2 matrices \
+    by using numpy
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+
+    return np.dot(m_a, m_b)
