e31c6ff93a10b421c9a6b2ae2564d6f122c12797
haddyadnan <haddyadnan@gmail.com>
2022-06-30 02:02:52 +0100

    lazy mul

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -10,4 +10,10 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
 
+    """
+    Dot multiplication of m_a and m_b using numpy
+    Return:
+        (Array)
+    """
+
     return np.dot(m_a, m_b)
