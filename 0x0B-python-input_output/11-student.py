e0f77b3cdb74c6c057c96362f4fad0d3e93e6fb4
haddyadnan <haddyadnan@gmail.com>
2022-07-06 14:50:17 +0100

    pycodestyle

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -49,11 +49,3 @@ class Student:
             self.first_name = json["first_name"]
             self.last_name = json["last_name"]
         return self
-
-
-# j_student_1 = {"first_name": "John", "last_name": "Lot", "age": 45}
-# new_student_1 = Student("Fake", "Fake", 89)
-# new_student_1.reload_from_json(j_student_1)
-# print(new_student_1)
-# print(type(new_student_1))
-# # print("{} {} {}".format(new_student_1.first_name, new_student_1.last_name, new_student_1.age))
