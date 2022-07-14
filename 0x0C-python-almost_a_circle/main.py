e0d303191216404de404a9dc43e3bc173bc27571
haddyadnan <haddyadnan@gmail.com>
2022-07-14 18:07:07 +0100

    updated display method to incl x and y axis

--- 0x0C-python-almost_a_circle/main.py
+++ 0x0C-python-almost_a_circle/main.py
@@ -3,11 +3,11 @@
 from models.rectangle import Rectangle
 
 if __name__ == "__main__":
-    
-   if __name__ == "__main__":
-    
-    r1 = Rectangle(4, 6, 2, 1, 12)
-    print(r1)
 
-    r2 = Rectangle(5, 5, 1)
-    print(r2)
+    r1 = Rectangle(2, 3, 2, 2)
+    r1.display()
+
+    print("---")
+
+    r2 = Rectangle(3, 2, 1, 0)
+    r2.display()
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -142,8 +142,10 @@ class Rectangle(Base):
         Return: None
         """
 
+        print("\n" * self.__y,end="")
         for i in range(self.__height):
-            print("#" * self.__width)
+
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self) -> str:
 
