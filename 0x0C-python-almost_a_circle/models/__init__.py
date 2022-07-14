7f8e87c20880b2a37a2acc1cf1d53355ce4a919a
haddyadnan <haddyadnan@gmail.com>
2022-07-14 14:53:35 +0100

    create base.py

---
+++ 0x0C-python-almost_a_circle/models/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+"""
+this module contains the init classs
+"""
+
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This module contains the Base class of all other classes in the project
+"""
+
+class Base:
+
+    """
+    Base class for other classses
+    """
+
+    __nb_objects = 0
+    def __init__(self, id=None) -> None:
+        
+        """
+        init the base class
+        """
+
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
