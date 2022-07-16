9c48c7e03eeafc44999b09420778494a7e5142a7
haddyadnan <haddyadnan@gmail.com>
2022-07-16 10:13:50 +0100

    added to_dict staticmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,9 @@ This module contains the Base class of all other classes in the project
 """
 
 
+import json
+
+
 class Base:
 
     """
@@ -24,3 +27,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+
+        """
+        returns JSON string representation of list_dictiomaries
+        """
+
+        if list_dictionaries is None or any(list_dictionaries) is False:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
