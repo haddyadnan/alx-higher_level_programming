2edf2721618d04d190e7b9a4a983cc936ed6b66e
haddyadnan <haddyadnan@gmail.com>
2022-08-11 18:30:10 +0100

    sql auth

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,5 +5,5 @@
 --         name VARCHAR(256) can’t be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Create table
-CREATE  TABLE IF NOT EXISTS hbtn_0d_usa.states
+CREATE  TABLE hbtn_0d_usa.states
 (id INT UNIQUE AUTO_INCREMENT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
