aae624bd8b9183aa352acd98b2a4084b7614a4dd
haddyadnan <haddyadnan@gmail.com>
2022-08-11 18:18:29 +0100

    turn query to upper

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -3,7 +3,7 @@
 --     Each record should display: tv_genres.name - rating sum
 --     Results must be sorted in descending order by their rating
 --     You can use only one SELECT statement
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 INNER JOIN tv_show_ratings ON tv_show_genres.show_id=tv_show_ratings.show_id
