f5a2c17d926d2136de0e3ac06e809441a187d551
haddyadnan <haddyadnan@gmail.com>
2022-09-07 02:34:51 +0100

    js oop

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -4,8 +4,7 @@ const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
   constructor (size) {
-    super(Rectangle.width, Rectangle.height);
-    this.width = this.height = size;
+    super(size, size);
   }
 }
 
