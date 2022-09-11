3150eb8a49a2f4ac999211d937d02834b504c7bf
haddyadnan <haddyadnan@gmail.com>
2022-09-11 17:06:18 +0100

    sql injection

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa
+where name matches the argument."""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8",
+    )
+    c = db.cursor()
+    c.execute(
+        """SELECT *
+                FROM states
+                WHERE name LIKE '{}'
+                ORDER BY id ASC""".format(
+            argv[4]
+        )
+    )
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa
+where name matches the argument
+"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8",
+    )
+    c = db.cursor()
+    param = argv[4].split(";")[0]
+    c.execute(
+        """SELECT *
+                FROM states
+                WHERE name LIKE '{}'
+                ORDER BY id ASC""".format(
+            param
+        )
+    )
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    c.close()
+    db.close()
